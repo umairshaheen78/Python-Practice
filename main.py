c75e165d2d2ce67b4f5257447627c3ada3041c60
Umair Shaheen <109942@eesd.org>
2021-02-11 04:38:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+name = input('What is your name?\n')
+print ("Hello" , name + "!")
+print ("Umair" , "likes to do coding")
+'''
+
+num = int(input('Enter a random number\n'))
+if (num > 10):
+  print ("Your number is bigger than 10")
+
+elif (num < 10):
+  print ("Your number is less than 10")
